4bfe82924f701dadba41d165fc5ff57e63752bd5
alumno <alumno>
2019-02-04 22:14:32 -0300

    comentario del commit

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,17 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
-}
+	var nombre = prompt("Por favor ingrese su nombre", "Natalia Natalia");
+    
+    alert (nombre);
 
+//comentarios
+erijei
+/*coment
+doemne
+coejhwjheh
+*/ejeii
+
+
+
+}
