d976c0c3ecf5d9d156adda22042f7f0be31b0180
alumno <alumno>
2019-02-06 21:03:15 -0300

    ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,60 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+
+//version uno	bien, pero con mismos nombres, confuso y variables puestas al pedo. pero funciona.
+
+	/*
+	var dividendo;
+	var divisor;
+	var resto;
+
+
+	numeroDividendo=numeroDividendo.value;
+	numeroDivisor=numeroDivisor.value;
+
+	numeroDividendo=parseInt(numeroDividendo);
+	numeroDivisor=parseInt(numeroDivisor);
+ 	
+ 	resto=numeroDividendo%numeroDivisor;
+
+	alert("el resto es " +resto);
+
+	*/
+
+//version dos  bien. mas prolijo y correcto.
 	
+	var dividendo;
+	var divisor;
+	var resto;
+
+
+	dividendo=numeroDividendo.value;
+	divisor=numeroDivisor.value;
+
+	numeroDividendo=parseInt(dividendo);
+	numeroDivisor=parseInt(divisor);
+ 	
+ 	resto=numeroDividendo%numeroDivisor;
+
+	alert("el resto es " +resto);
+	
+
+//version tres (mal)
+	/*
+	var dividendo;
+	var divisor;
+	var resto;
+
+
+	dividendo=numeroDividendo.value;
+	divisor=numeroDivisor.value;
+
+	dividendo=parseInt(numeroDividendo);
+	divisor=parseInt(numeroDivisor);
+ 	
+ 	resto=numeroDividendo%numeroDivisor;
+
+	alert("el resto es " +resto);
+	*/
 }
