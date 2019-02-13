c7b9046095bc522580f7d31814173a9a96d6ce05
alumno <alumno>
2019-02-13 19:18:38 -0300

    primer ejercicio de parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,6 +1,37 @@
 
 function mostrar()
 {
+	
+	var nombreUno;
+	var nombreDos;
+	var edadUno;
+	var edadDos;
+	var sumaEdades;
+
+	nombreUno=prompt("Ingrese el nombre de la primera persona","nombre y apellido");
+	nombreDos=prompt("Ingrese el nombre de la segunda persona","nombre y apellido");
+	edadUno=prompt("Ingrese la edad de la primera persona","edad");
+	edadDos=prompt("Ingrese la edad de la segunda persona","edad");
+
+
+	edadUno=parseInt(edadUno);
+	edadDos=parseInt(edadDos);
+
+	sumaEdades=edadUno+edadDos
+
+	alert("Ustedes son "+nombreUno+" y "+nombreDos+" , sus edades son "+edadUno+" y "+edadDos+" , y la suma de sus edades es "+sumaEdades);
+
+
+
+
+
+
+
+
+
+
+
+/*
 	var ancho;
 	var largo;
 	var permietro;
@@ -16,5 +47,5 @@ function mostrar()
 	alert("el permetro es "+perimetro);
 
 		
-
+*/
 }
