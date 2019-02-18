7ad63b113a7213e96b09e2ba3e7e22989fc82b35
alumno <alumno>
2019-02-18 20:46:56 -0300

    ejercicios if

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+	var planeta;
+	var mensaje;
+
+	planeta=prompt("Ingrese un planeta del sistema solar");
+	mensaje="este no es un planeta válido";
+
+
+	switch(planeta)
+	{
+		case "tierra":
+		mensaje="aca vivimos";
+		break;
+		
+		case "mercurio":
+		case "venus":
+		case "marte":
+		case "jupiter":
+		mensaje="aca hace mas calor";
+		break;
+
+		case "saturno":
+		case "urano":
+		case "neptuno":
+		mensaje="aca hace mas frio";
+		break;
+	}
+
+
+	alert(mensaje);
 
 }
