196c986db0b414dd277a88837618f4fe126abc87
alumno <alumno>
2019-02-18 22:05:41 -0300

    el while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -4,4 +4,12 @@ function mostrar()
 var clave = prompt("ingrese el número clave.");
 
 
+	while(clave!="utn750")
+	{
+		//aca se piensa en base a la incorrecta
+		clave=prompt("ingrese el numero clave");
+
+	}
+	alert("bien, entraste")
+
 }//FIN DE LA FUNCIÓN
