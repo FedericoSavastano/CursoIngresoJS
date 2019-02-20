a54e2da3a915f8e336f7ebe929082a40a239583b
unknown <you@example.com>
2019-02-20 03:25:05 -0300

    parcial recu

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var concatenados;
+	var division;
+	var suma;
 
-}
+
+	numeroUno=prompt("Ingrese el primer numero");
+	numeroDos=prompt("Ingrese el segundo numero");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	concatenados=(""+numeroUno+" y "+numeroDos);
+	division=numeroUno/numeroDos;
+	suma=numeroUno+numeroDos;
+
+	if(numeroUno=numeroDos)
+	{
+		mensaje=concatenados;
+	
+	if(numeroUno>numeroDos)
+		{
+			mensaje=division
+		}
+		else
+		{
+		mensaje=suma
+			if(suma<50)
+			{
+				mensaje=("la suma es "+suma+" y es menor a 50");
+			}
+		
+		}
+
+	}
+
+	alert(mensaje);
+	
+}	
