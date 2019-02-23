397824dd138e4d3ef1c1315e9f699e1469955c0e
yenifer02101 <yenny_1892@hotmail.com>
2019-02-23 13:06:10 -0300

    clase saba while

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,6 +1,42 @@
 function mostrar()
 {
 
+	var clave = prompt("ingrese la clave");
+	var intentos = 0;
+	var flag = false;
+
+/*
+	while(clave!="UTN750")
+	{
+		alert("Clave incorrecta. Reingrese");
+		clave = prompt("ingrese la clave");
+	}
+
+*/
+	while(clave!="UTN750")
+	{
+		alert("Clave incorrecta. Reingrese");
+		clave = prompt("ingrese la clave");
+		intentos=intentos + 1;
+		if(intentos==3) //con esto pide 4 veces total
+		{
+			flag = true;
+			break;
+		}
+	}
+
+	if(flag==false)
+	{
+		alert("bienvenido al sistema");
+	}
+	
+
+
+
+
+
+
+/*
 var clave = prompt("ingrese el número clave.");
 
 
@@ -11,5 +47,6 @@ var clave = prompt("ingrese el número clave.");
 
 	}
 	alert("bien, entraste")
+	*/
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,68 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
-	var respuesta='si';
+	var numero;
+	var maximo;
+	var minimo;
+	var cantidad=0;
+	var flag=false;
 
+	var respuesta='si';
+/*
 	while(respuesta!='no')
 	{
-		
+		numero=prompt("Ingrese numero");
+
+
+		if(cantidad == 0)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero>minimo)
+			{
+				minimo=numero;
+			}
+		}
+
+		cantidad++;
+		respuesta=prompt("desea ingresar otro numero?");
 	
 	}
 
+	document.getElementById. 
+*/
 
 
+	while(respuesta!='no')
+	{
+		numero=prompt("Ingrese numero");
+
+
+		if(flag==false || numero>maximo)
+		{
+			maximo=numero;
+			//flag=true; esto aca no iria, porque te haria al pedo lo de abajo
+		}
+		if(flag==false || numero<minimo)
+		{
+			minimo=numero;
+			flag=true;
+		}
+
+
+
+		cantidad++;
+		respuesta=prompt("desea ingresar otro numero?");
+	
+	}
+
+	document.getElementById.("minimo").value=minimo;
+	idem maximo
 
 }//FIN DE LA FUNCIÓN
