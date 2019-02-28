6e0d061bce708646bb085e33b274523b028c3b42
alumno <alumno>
2019-02-27 22:10:26 -0300

    clase mier 27 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+	var nota
+	var sexo
+	var contador=0;
+	var contVarMasSeis=0;
+	var acumuladorNota=0;
+	var sexoNotaMasBaja;
+	var notaMasBaja;
+	var
+
+
+	while(contador>5)
+	{
+		contador++;
+		nota=prompt("ingrese la nota");
+		sexo=prompt("ingrese el sexo");
+		
+		if(nota>0 && nota<10)
+		{
+			nota=parseInt(nota);
+			acumuladorNota=acumuladorNota+nota;
+		}
+
+
+		if(nota<notaMasBaja)
+		{
+			notaMasBaja=nota;
+			sexo=sexoNotaMasBaja;
+		}
+
+
+		if(nota>=6 && sexo=="m")
+		{
+			contVarMasSeis++;
+		}
+	}
+
+
+
+	promedio=acumulador/5;
+
+	alert("el promedio de las notas es "+promedio+"la nota mas baja es "+notaMasBaja+" su sexo es "+sexoNotaMasBaja+" la cantidad de varones con 6 es "+contVarMasSeis);
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,71 @@
 function mostrar()
-{
+{	
+	var animal;
+	var peso;
+	var temperatura;
+	var contador=0;
+	var contadorDePeso=0;
+	var acumuladorPeso=0;
+	var contTempPares=0;
+	var contAnimalesBajoCero=0;
+
+	var acumuladorPesoBajoCero=0;
+	var respuesta="si";
+
+	while(respuesta=="si")
+	{
+		contador++;
+		animal=prompt("Ingrese el animal");
+		peso=prompt("Ingrese el peso");
+		temperatura=prompt("Ingrese temperatura");
+
+
+		//cantidad de temperaturas pares
+		//La cantidad de animales que viven a menos de 0 grados. 
+
+		if(temperatura>-30 && temperatura<30)
+		{
+			temperatura=parseInt(temperatura);
+
+			if(temperatura%2 ==0)
+			{
+				contTempPares++;
+			}
+
+			if(temperatura<0)
+			{
+				contAnimalesBajoCero++;
+
+				acumuladorPeso=peso+acumuladorPesoBajoCero;
+			}
+		}
+		
+		
+		//promedio del peso de todos los animales
+		if(peso>1 && peso<1000)
+		{
+			peso=parseInt(peso);
+			//contadorDePeso++;
+			acumuladorPeso=peso+acumuladorPeso;
+		}
+		
+
+
+
+
+		respuesta=prompt("desea continuar?");
+	}
+
+
+
+
+	promedioPeso=acumuladorPeso/contador;
+
+
+	alert("El promedio de pesos es "+promedioPeso);
+	alert("la cantidad de animales que viven bajo cero es "+contAnimalesBajoCero);
+	alert("temperaturas pares son "+contTempPares);
+
+
 
 }
